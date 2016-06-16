b77e2aaac86f3fe5cc729ead520269ab98361363
yamadapc <tacla.yamada@gmail.com>
2016-06-16 17:17:44 -0300

    Workspace update

--- examples/Main.hs
+++ examples/Main.hs
@@ -2,11 +2,11 @@ import           Control.Concurrent
 import           GHCJS.Marshal.Pure
 import           GHCJS.Require
 
-helloWorld = putStrLn "Hello World"
+helloWorld = putStrLn "[haskell] Hello World"
 
 launchTheMissiles = do
     threadDelay (1000 * 1000 * 5)
-    putStrLn "OMG what did I do?!"
+    putStrLn "[haskell] OMG what did I do?!"
     return $ Just $ pToJSVal (10 :: Double)
 
 main = do
--- examples/Main.js
+++ examples/Main.js
@@ -3,13 +3,14 @@ const main = ghcjsRequire('./Main.jsexe')();
 
 main.emitter.once('ghcjs-require:loaded', () => {
   console.log('[javascript] Haskell runtime loaded!');
+  console.log('[javascript] Calling helloWorld...');
   main.exports.helloWorld();
 
-  console.log('[javascript] Sending message...');
+  console.log('[javascript] Sending message to call launchTheMissiles...');
   main.emitter.emit('ghcjs-require:runexport', 'launchTheMissiles', (err, x) => {
     console.log(`[javascript] launchTheMissiles ended yielding: ${x}`);
 
-    console.log('[javascript] Running as wrapped function that returns promise');
+    console.log('[javascript] Running launchTheMissiles as wrapped function that returns promise');
     main.wrapped.launchTheMissiles().then((x) => {
       console.log(`[javascript] launchTheMissiles promised ended yielding: ${x}`);
     });
