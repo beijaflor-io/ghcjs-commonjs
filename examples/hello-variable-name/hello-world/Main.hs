f617e1c417427293851d2dab6d69a5d266301618
yamadapc <tacla.yamada@gmail.com>
2016-06-18 12:58:19 -0300

    Update examples

--- examples/hello-variable-name/hello-world/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import qualified GHCJS.CommonJS as CommonJS
-
-main :: IO ()
-main = CommonJS.exportMain
-    [ CommonJS.pack ("sayHello", putStrLn "Hello, I'm a Haskell")
-    ]
--- examples/hello-variable-name/hello-world/index.js
+++
@@ -1,8 +0,0 @@
-const ghcjsRequire = require('ghcjs-require');
-const HelloWorld = ghcjsRequire(module, 'ghcjs-commonjs-hello-world');
-
-HelloWorld(({wrapped}) => {
-  wrapped.sayHello().then(() => {
-    console.log('And I\'m a JavaScript');
-  });
-});
