57d66afd310c8c0785d11bf6521536e51a577d14
yamadapc <tacla.yamada@gmail.com>
2016-06-20 14:55:47 -0300

    Update fibonacci to the latest API

--- examples/fibonacci/Main.hs
+++ examples/fibonacci/Main.hs
@@ -1,18 +1,16 @@
 module Main where
 
-import qualified GHCJS.CommonJS as CommonJS
+import           GHCJS.CommonJS (exportMain, exports)
 
 fibs :: [Int]
 fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
 
 main :: IO ()
-main = CommonJS.exportMain
-    [ CommonJS.pack ("fibs", \n -> take n fibs)
-    -- ^ We can "CommonJS.pack" pure functions!
-    , CommonJS.pack
-        ("fibsIO", \n -> do
-                putStrLn ("HASKELL LAND - Calculating fibs of " ++ show n)
-                return (take n fibs)
-        )
+main = exportMain
+    [ "fibs" `exports` \n -> take n fibs
+    -- ^ We can "exports" pure functions!
+    , "fibsIO" `exports` \n -> do
+            putStrLn ("HASKELL LAND - Calculating fibs of " ++ show n)
+            return (take n fibs)
     -- ^ And IO still works as expected
     ]
