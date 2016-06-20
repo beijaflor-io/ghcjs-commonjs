c5a390d7fc87cd06184bdecdbdf9ff0283cb6ca0
yamadapc <tacla.yamada@gmail.com>
2016-06-20 14:31:49 -0300

    Remove logging statement

--- ghcjs-register/ghcjs-register.js
+++ ghcjs-register/ghcjs-register.js
@@ -3,7 +3,6 @@ const ghcjsRequire = require('ghcjs-require');
 
 require.extensions['.hs'] = function(module, fp) {
   const jsexePath = path.join(path.dirname(fp), path.basename(fp, '.hs') + '.jsexe');
-  console.log(jsexePath);
   return module._compile(
     ghcjsRequire.generateWrapper(jsexePath),
     fp
